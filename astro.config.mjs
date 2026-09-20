1b7cab9d8681b95a776f902946bfb2137bdc801b
pichugster <spb.pichugin.n@gmail.com>
2026-09-20 13:30:37 +0700

    индекс

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://priehali.com',
   adapter: vercel(),
+  integrations: [sitemap()],
 });
