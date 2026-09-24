16eada4b3bdd4a8de43a33cf22338a073190d250
pichugster <spb.pichugin.n@gmail.com>
2026-09-24 08:34:29 +0700

    замена админка

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import sitemap from '@astrojs/sitemap';
+import decapCmsOauth from 'astro-decap-cms-oauth';
 
 export default defineConfig({
   site: 'https://priehali.com',
   adapter: vercel(),
-  integrations: [sitemap()],
+  integrations: [sitemap(), decapCmsOauth()],
 });
